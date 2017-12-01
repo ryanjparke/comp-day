757ee4963e8a8d9efc7e613386687dee526a416a
Ryan Parke <ryanjparke94@gmail.com>
2017-12-01 13:43:54 -0700

    server working and dotenv created

---
+++ Server/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    massive = require('massive'),
+    cors = require('cors'),
+    axios = require('axios');
+
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// massive(process.env.CONNECTION_STRING).then(dbInstance =>app.set('db', dbinstance));
+
+//Endpoints
+
+
+
+
+
+
+//Listen port
+const PORT = 3005;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
